22f4a1a34601cc6664d42fe32839a9f971b7f000
Ángel Luis Hernández Herrero <angelluishernandezherrero@gmail.com>
2020-07-31 17:00:46 +0200

    feat: gatsby node updated

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,71 +1,71 @@
-const path = require(`path`)
-const { createFilePath } = require(`gatsby-source-filesystem`)
+// const path = require(`path`)
+// const { createFilePath } = require(`gatsby-source-filesystem`)
 
-// Create pages
+// // Create pages
 
-exports.createPages = async ({ graphql, actions }) => {
-  const { createPage } = actions
+// exports.createPages = async ({ graphql, actions }) => {
+//   const { createPage } = actions
 
-  const noticiasTemplate = path.resolve(`./src/templates/noticias-item.js`)
-  const response = await graphql(
-    `
-      {
-        allMarkdownRemark(
-          sort: { fields: [frontmatter___date], order: DESC }
-          limit: 1000
-        ) {
-          edges {
-            node {
-              fields {
-                slug
-              }
-              frontmatter {
-                titulo
-                subtitulo
-                cuerpo
-              }
-            }
-          }
-        }
-      }
-    `
-  )
+//   const noticiasTemplate = path.resolve(`./src/templates/noticias-item.js`)
+//   const response = await graphql(
+//     `
+//       {
+//         allMarkdownRemark(
+//           sort: { fields: [frontmatter___date], order: DESC }
+//           limit: 1000
+//         ) {
+//           edges {
+//             node {
+//               fields {
+//                 slug
+//               }
+//               frontmatter {
+//                 titulo
+//                 subtitulo
+//                 cuerpo
+//               }
+//             }
+//           }
+//         }
+//       }
+//     `
+//   )
 
-  if (response.errors) {
-    throw response.errors
-  }
+//   if (response.errors) {
+//     throw response.errors
+//   }
 
-  // Create blog newsItem pages.
-  const news = response.data.allMarkdownRemark.edges
+//   // Create blog newsItem pages.
+//   const news = response.data.allMarkdownRemark.edges
 
-  news.forEach((newsItem, index) => {
-    const previous = index === news.length - 1 ? null : news[index + 1].node
-    const next = index === 0 ? null : news[index - 1].node
+//   news.forEach((newsItem, index) => {
+//     const previous = index === news.length - 1 ? null : news[index + 1].node
+//     const next = index === 0 ? null : news[index - 1].node
 
-    createPage({
-      path: `noticias/${newsItem.node.fields.slug}`,
-      component: noticiasTemplate,
-      context: {
-        slug: newsItem.node.fields.slug,
-        previous,
-        next,
-      },
-    })
-  })
-}
+//     createPage({
+//       path: `/noticias/${newsItem.node.fields.slug}`,
+//       component: noticiasTemplate,
+//       context: {
+//         slug: newsItem.node.fields.slug,
+//         previous,
+//         next,
+//       },
+//     })
+//   })
+// }
 
-// Create node pagess
+// // Create node pagess
 
-exports.onCreateNode = ({ node, actions, getNode }) => {
-  const { createNodeField } = actions
+// exports.onCreateNode = ({ node, actions, getNode }) => {
+//   const { createNodeField } = actions
 
-  if (node.internal.type === `MarkdownRemark`) {
-    const value = createFilePath({ node, getNode })
-    const slug = path.basename(node.fileAbsolutePath, ".md")
-    createNodeField({
-      name: `slug`,
-      node,
-      value: slug,
-    })
-  }
-}
+//   if (node.internal.type === `MarkdownRemark`) {
+//     const value = createFilePath({ node, getNode })
+//     const slug = path.basename(node.fileAbsolutePath, ".md")
+//     createNodeField({
+//       name: `slug`,
+//       node,
+//       value: slug,
+//     })
+//   }
+// }
--- src/templates/noticias-item.js
+++ src/templates/noticias-item.js
@@ -1,29 +1,29 @@
-import React from "react"
-import Layout from "../components/layout"
-import { graphql } from "gatsby"
+// import React from "react"
+// import Layout from "../components/layout"
+// import { graphql } from "gatsby"
 
-// export const query = graphql`
-//   query($slug: String!) {
-//     markdownRemark(fields: { slug: { eq: $slug } }) {
-//       frontmatter {
-//         title
-//         date
-//       }
-//       html
-//     }
-//   }
-// `
+// // export const query = graphql`
+// //   query($slug: String!) {
+// //     markdownRemark(fields: { slug: { eq: $slug } }) {
+// //       frontmatter {
+// //         title
+// //         date
+// //       }
+// //       html
+// //     }
+// //   }
+// // `
 
-const NoticiasItem = props => {
-  return (
-    <Layout>
-      <h1>{props.data.markdownRemark.frontmatter.title}</h1>
-      <p>{props.data.markdownRemark.frontmatter.date}</p>
-      <div
-        dangerouslySetInnerHTML={{ __html: props.data.markdownRemark.html }}
-      ></div>
-    </Layout>
-  )
-}
+// const NoticiasItem = props => {
+//   return (
+//     <Layout>
+//       <h1>{props.data.markdownRemark.frontmatter.title}</h1>
+//       <p>{props.data.markdownRemark.frontmatter.date}</p>
+//       <div
+//         dangerouslySetInnerHTML={{ __html: props.data.markdownRemark.html }}
+//       ></div>
+//     </Layout>
+//   )
+// }
 
-export default NoticiasItem
+// export default NoticiasItem
